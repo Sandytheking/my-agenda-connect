34dd8ce8c15e54506bc2f6cb09cbd15b0031362c
Sandytheking <sandytheking@hotmail.com>
2025-08-01 16:42:59 -0400

    Mejoras agregadas

--- app/registro/page.tsx
+++ app/registro/page.tsx
@@ -2,12 +2,16 @@
 'use client';
 
 import { Suspense } from 'react';
-import RegistroForm from './RegistroForm';
+import RegistroForm from "./RegistroForm";
 
 export default function RegistroPage() {
   return (
-    <Suspense fallback={<div>Cargando...</div>}>
-      <RegistroForm />
-    </Suspense>
+<div className="min-h-screen bg-black text-white flex items-center justify-center pt-28 px-4">
+      <div className="max-w-md w-full bg-gray-900 text-white p-8 rounded-2xl shadow-lg">
+        <RegistroForm />
+      </div>
+    </div>
+
+
   );
 }
