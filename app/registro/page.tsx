2a9c231c430a1fe7d07b98bd14059cbc7edd6c5f
Sandytheking <sandytheking@hotmail.com>
2025-08-02 06:47:08 -0400

    Mejoras agregadas

--- app/registro/page.tsx
+++ app/registro/page.tsx
@@ -6,12 +6,12 @@ import RegistroForm from "./RegistroForm";
 
 export default function RegistroPage() {
   return (
-<div className="min-h-screen bg-black text-white flex items-center justify-center pt-28 px-4">
-      <div className="max-w-md w-full bg-gray-900 text-white p-8 rounded-2xl shadow-lg">
-        <RegistroForm />
+    <div className="min-h-screen bg-black text-white flex items-center justify-center pt-28 px-4">
+  <div className="max-w-md w-full bg-black text-white p-8 rounded-2xl shadow-lg">
+        <Suspense fallback={<div>Cargando...</div>}>
+          <RegistroForm />
+        </Suspense>
       </div>
     </div>
-
-
   );
 }
