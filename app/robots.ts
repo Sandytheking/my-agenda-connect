185cba620913ca29947ef472d15b0f1f3340f5ee
Sandytheking <sandytheking@hotmail.com>
2026-01-18 13:48:47 -0400

    Se edito el sitemap

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://agenda-connect.com/sitemap.xml",
+    sitemap: "https://www.agenda-connect.com/sitemap.xml",
   };
 }
